4cda8dd664dde51b5cb62fe9705c4f3bf489a7cb
kimnoca <kimnoca@naver.com>
2023-09-21 17:31:27 +0900

    Feet: Add UserManager Class

--- gathering/models.py
+++ gathering/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+# User model 을 위한 헬퍼 클래스
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, nickname, **kwargs):
+        user = self.model(email=email, nickname=nickname)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+
+# User model
+class User(AbstractBaseUser):
+    pass
