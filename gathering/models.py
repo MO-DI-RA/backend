22e5720100acc7727686f3a9a30d9009b554144e
kimnoca <kimnoca@naver.com>
2023-09-22 17:08:30 +0900

    Feat: Add User model

--- gathering/models.py
+++ gathering/models.py
@@ -1,16 +1,57 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,  # superuser 라는 권한이 생겼기 때문에 추가해 줘야함
+)
 
 
 # User model 을 위한 헬퍼 클래스
 class UserManager(BaseUserManager):
     def create_user(self, email, password, nickname, **kwargs):
+        if not email:
+            raise ValueError("User must have email address")
+
         user = self.model(email=email, nickname=nickname)
         user.set_password(password)
         user.save(using=self.db)
         return user
 
+    def create_superuser(self, email, password, nickname, **extra_fields):
+        superuser = self.create_user(
+            email=email,
+            password=password,
+            nickname=nickname,
+        )
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.is_active = True
+
+        superuser.save(using=self._db)
+        return superuser
+
+    def __str__(self):
+        return self.nickname
+
 
 # User model
-class User(AbstractBaseUser):
-    pass
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        max_length=50, unique=True, null=False, blank=False
+    )  # email
+    nickname = models.CharField(
+        max_length=20, unique=True, null=False, blank=False
+    )  # nickname
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()  # 헬퍼 클래스 지정
+
+    USERNAME_FIELD = "email"  # email로 로그인 설정
+
+    REQUIRED_FIELDS = [  # USERNAME_FIELD, Password는 항상 기본적으로 요구 따로 명시 x
+        "nickname",
+    ]
