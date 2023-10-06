a1a2c63e27c0a889e8ff457b5efdd81ed4a177c6
kimnoca <kimnoca@naver.com>
2023-10-06 12:14:52 +0900

    Feat: Add Login

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RegisterAPIView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterAPIView, LoginAPIView
 
 urlpatterns = [
-    path("register/", RegisterAPIView.as_view()),  # post - 회원가입
+    path("signup/", RegisterAPIView.as_view()),  # post - 회원가입
+    path("login/", LoginAPIView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -3,11 +3,13 @@ from .serializers import *
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 
 
 class RegisterAPIView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
+
         if serializer.is_valid():
             user = serializer.save()
 
@@ -28,8 +30,41 @@ class RegisterAPIView(APIView):
             )
 
             # jwt 토큰 => 쿠키에 저장
-            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("access  ", access_token, httponly=True)
             res.set_cookie("refresh", refresh_token, httponly=True)
-
+            # print(res)
             return res
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        # 유저 인증
+        user = authenticate(
+            email=request.data.get("email"), password=request.data.get("password")
+        )
+        # 이미 회원가입 된 유저일 때
+        if user is not None:
+            serializer = UserSerializer(user)
+            # jwt 토큰 접근
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "login success",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            # jwt 토큰 => 쿠키에 저장
+            print(refresh_token)
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
