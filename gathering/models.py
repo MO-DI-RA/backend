158784307879e896296162a8e1544d1492f21617
guntaek <rjsxor7584@yu.ac.kr>
2023-10-08 19:09:47 +0900

    Add Gathering models.py, serializers.py

---
+++ gathering/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .user.model import User
+
+class Gathering(models.Model):
+    author_id = models.ForeignKey(User, related_name = 'users', on_delete = models.CASCADE,)
+    title = models.CharField(verbose_name = 'title', max_length = 64,)
+    content = models.CharField(verbose_name = 'content', max_length = 2000,)
+    created_at = models.DateTimeField(verbose_name = 'created at', auto_now_add = Ture)
+    updated_at = models.DateTimeField(verbose_name = 'updated at', auto_now = True)
+
+class Comment(models.Model):
+    author_id = models.ForeignKey(User, related_name = 'users', on_delete = models.CASCADE,)
+    post_id = models.ForeignKey('gathering.Gathering',verbose_name = 'gatherings', on_delete = True)
+    content = models.CharField(verbose_name = 'comment', max_length = 300,)
+    created_at = models.DateTimeField(verbose_name = 'created at', auto_now_add = Ture)
+    updated_at = models.DateTimeField(verbose_name = 'updated at', auto_now = True)
