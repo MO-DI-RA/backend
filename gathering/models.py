3ed52316474102e5969c35f263d7727c3165f9af
kimnoca <kimnoca@naver.com>
2023-10-08 22:12:21 +0900

    Fix: Model 재작성

--- gathering/models.py
+++ gathering/models.py
@@ -2,17 +2,16 @@ from django.db import models
 from users.models import User
 
 
-class Gathering(models.Model):
+class GatheringPost(models.Model):
     author_id = models.ForeignKey(
         User,
-        related_name="users",
         on_delete=models.CASCADE,
     )
     title = models.CharField(
         verbose_name="title",
         max_length=64,
     )
-    content = models.CharField(
+    content = models.TextField(
         verbose_name="content",
         max_length=2000,
     )
@@ -27,7 +26,7 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
     )
     post_id = models.ForeignKey(
-        "gathering.Gathering", verbose_name="gatherings", on_delete=True
+        GatheringPost, verbose_name="gatherings", on_delete=models.CASCADE
     )
     content = models.CharField(
         verbose_name="comment",
--- modira/settings.py
+++ modira/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "corsheaders",
     # My app
     "users",
+    "gathering",
 ]
 
 MIDDLEWARE = [
