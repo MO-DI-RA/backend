f1a6765c3c11a04f73e94210d9dc800b0931cd8e
khj <hhbb0230@naver.com>
2023-10-31 19:49:24 +0900

    Rename: GatheringPosts에서 GatheringPost로 변경후 migration

--- gathering/models.py
+++ gathering/models.py
@@ -1,77 +1,18 @@
 from django.db import models
-<<<<<<< HEAD
-from django.contrib.auth.models import (
-    BaseUserManager,
-    AbstractBaseUser,
-    PermissionsMixin,  # superuser 라는 권한이 생겼기 때문에 추가해 줘야함
-)
-
-
-# User model 을 위한 헬퍼 클래스
-class UserManager(BaseUserManager):
-    def create_user(self, email, password, nickname, **kwargs):
-        if not email:
-            raise ValueError("User must have email address")
-
-        user = self.model(email=email, nickname=nickname)
-        user.set_password(password)
-        user.save(using=self.db)
-        return user
-
-    def create_superuser(self, email, password, nickname, **extra_fields):
-        superuser = self.create_user(
-            email=email,
-            password=password,
-            nickname=nickname,
-        )
-        superuser.is_staff = True
-        superuser.is_superuser = True
-        superuser.is_active = True
-
-        superuser.save(using=self._db)
-        return superuser
-
-    def __str__(self):
-        return self.nickname
-
-
-# User model
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(
-        max_length=50, unique=True, null=False, blank=False
-    )  # email
-    nickname = models.CharField(
-        max_length=20, unique=True, null=False, blank=False
-    )  # nickname
-    is_active = models.BooleanField(default=True)
-
-    is_staff = models.BooleanField(default=False)
-
-    is_superuser = models.BooleanField(default=False)
-    
-    user_created_at = models.DateTimeField(auto_now=True)
-
-    objects = UserManager()  # 헬퍼 클래스 지정
-
-    USERNAME_FIELD = "email"  # email로 로그인 설정
-
-    REQUIRED_FIELDS = [  # USERNAME_FIELD, Password는 항상 기본적으로 요구 따로 명시 x
-        "nickname",
-    ]
-=======
 from users.models import User
 
 
-class GatheringPost(models.Model):
+class Gathering(models.Model):
     author_id = models.ForeignKey(
         User,
+        related_name="users",
         on_delete=models.CASCADE,
     )
     title = models.CharField(
         verbose_name="title",
         max_length=64,
     )
-    content = models.TextField(
+    content = models.CharField(
         verbose_name="content",
         max_length=2000,
     )
@@ -86,7 +27,7 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
     )
     post_id = models.ForeignKey(
-        GatheringPost, verbose_name="gatherings", on_delete=models.CASCADE
+        "gathering.Gathering", verbose_name="gatherings", on_delete=True
     )
     content = models.CharField(
         verbose_name="comment",
@@ -94,4 +35,3 @@ class Comment(models.Model):
     )
     created_at = models.DateTimeField(verbose_name="created at", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="updated at", auto_now=True)
->>>>>>> 1b3a0aa38059994d474bba1e3ca59857888f4fdc
