a1a4f293a413074c1347b6df6c079a9eecd79827
khj <hhbb0230@naver.com>
2023-10-31 23:53:59 +0900

    Fix: 단순 수정

--- gathering/views.py
+++ gathering/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# index.html 페이지를 부르는 index 함수
+def index(request):
+    return render(request, 'main/index.html')
+
+def gathering(request):
+    return render(request, 'main/test1.html')
