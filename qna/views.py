1b5aacf0946be5aa5b25783c8ed3b1e9d7cf14ac
jahyun <jahyungoo6979@gmail.com>
2023-11-03 21:25:12 +0900

    Feat: Add qna.views.py

--- qna/views.py
+++ qna/views.py
@@ -1,8 +1,8 @@
-from .models import GatheringPost, Comment
+from .models import QnA, Answer
 from .serializers import (
-    PostDetailSerializer,
-    PostListSerializer,
-    CommentSerializer,
+    QnADetailSerializer,
+    QnAListSerializer,
+    AnswerSerializer,
 )
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,12 +12,12 @@ from django.http import Http404
 
 class ListAPIView(APIView):  ## auther_id 도 나와야함
     def get(self, request):  # 모든 게시물
-        posts = GatheringPost.objects.all()
-        serializer = PostListSerializer(posts, many=True)
+        posts = QnA.objects.all()
+        serializer = QnAListSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        serializer = PostListSerializer(data=request.data)
+        serializer = QnAListSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -28,14 +28,14 @@ class ListAPIView(APIView):  ## auther_id 도 나와야함
 class PostAPIView(APIView):
     def get_object(self, pk):
         try:
-            return GatheringPost.objects.get(pk=pk)
-        except GatheringPost.DoesNotExist:
+            return QnA.objects.get(pk=pk)
+        except QnA.DoesNotExist:
             raise Http404
 
     # CRUD 중 R
     def get(self, request, pk, format=None):
         post = self.get_object(pk)
-        serializer = PostDetailSerializer(post)
+        serializer = QnADetailSerializer(post)
         return Response(serializer.data)
 
     # CRUD 중 U
@@ -43,7 +43,7 @@ class PostAPIView(APIView):
         post = self.get_object(pk)
         # print(request.user.id)
         if post.author_id.id == request.user.id:  # 작성자가 같을때만 수정 가능
-            serializer = PostDetailSerializer(post, data=request.data)
+            serializer = QnADetailSerializer(post, data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
