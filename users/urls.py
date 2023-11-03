796738d55539452fd9f262cc7637d6f461489340
kimnoca <kimnoca@naver.com>
2023-11-03 23:58:51 +0900

    feat : JWT 인증 추가

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from .views import (
     RegisterAPIView,
     LoginAPIView,
@@ -12,6 +12,7 @@ urlpatterns = [
     path("signup/", RegisterAPIView.as_view()),  # post - 회원가입
     path("login/", LoginAPIView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
+    path("verify/", TokenVerifyView.as_view()),
     path("mypage/", UserDetailAPIView.as_view()),
     path("kakao/login", kakao_login, name="kakao_login"),
     path("kakao/callback", kakao_callback, name="kakao_callback"),
