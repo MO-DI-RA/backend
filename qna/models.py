ff348db5a48019b2bdec45c39843b773e65d001d
jahyun <jahyungoo6979@gmail.com>
2023-11-04 03:58:33 +0900

    Feat : Add Answer Comment

--- qna/models.py
+++ qna/models.py
@@ -15,6 +15,7 @@ class QnAPost(models.Model):
         verbose_name="content",
         max_length=2000,
     )
+    status = models.BooleanField(verbose_name="status", default= False)
     created_at = models.DateTimeField(verbose_name="created at", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="updated at", auto_now=True)
 
@@ -28,6 +29,21 @@ class Answer(models.Model):
     )
     content = models.CharField(
         verbose_name="answer",
+        max_length=300,
+    )
+    created_at = models.DateTimeField(verbose_name="created at", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="updated at", auto_now=True)
+
+class AnswerComment(models.Model):
+    author_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    answer_id = models.ForeignKey(
+        Answer, verbose_name="answers", on_delete=models.CASCADE
+    )
+    content = models.CharField(
+        verbose_name="answercomment",
         max_length=300,
     )
     created_at = models.DateTimeField(verbose_name="created at", auto_now_add=True)
