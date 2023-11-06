ca48c209fa4f890ce28310be17d9e1bb69a79857
kimnoca <kimnoca@naver.com>
2023-11-06 16:48:29 +0900

    feat : 카카오 유저 정보 저장

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,26 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenObtainPairView,
+)
 from .views import (
     RegisterAPIView,
     LoginAPIView,
     UserDetailAPIView,
     kakao_callback,
     kakao_login,
+    KakaoLogin,
 )
 
 urlpatterns = [
     path("signup/", RegisterAPIView.as_view()),  # post - 회원가입
     path("login/", LoginAPIView.as_view()),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view()),
     path("verify/", TokenVerifyView.as_view()),
     path("mypage/", UserDetailAPIView.as_view()),
-    path("kakao/login", kakao_login, name="kakao_login"),
-    path("kakao/callback", kakao_callback, name="kakao_callback"),
+    path("kakao/login/", kakao_login, name="kakao_login"),
+    path("kakao/callback/", kakao_callback, name="kakao_callback"),
+    path("kakao/login/finish/", KakaoLogin.as_view()),
 ]
