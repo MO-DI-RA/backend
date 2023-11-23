dc403b654fe550379739b6c9a0c7971f2c8eb1fd
guntaek <rjsxor7584@yu.ac.kr>
2023-11-23 19:32:35 +0900

    Feat: gathering/views.py 수정

--- gathering/views.py
+++ gathering/views.py
@@ -41,7 +41,7 @@ class PostAPIView(APIView):
     # CRUD 중 U
     def put(self, request, pk, format=None):
         post = self.get_object(pk)
-        print(request.user.id)
+        # print(request.user.id)
         if post.author_id.id == request.user.id:  # 작성자가 같을때만 수정 가능
             serializer = PostDetailSerializer(post, data=request.data)
             if serializer.is_valid():
@@ -63,26 +63,6 @@ class PostAPIView(APIView):
             return Response(
                 {"message": "자신의 게시물이 아닙니다."}, status=status.HTTP_403_FORBIDDEN
             )
-
-
-class PostToggleStatus(APIView):
-    def get_object(self, pk):
-        try:
-            return GatheringPost.objects.get(pk=pk)
-        except GatheringPost.DoesNotExist:
-            raise Http404
-
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        if post.author_id.id == request.user.id:
-            post.status = not post.status
-            post.save()
-            return Response({"message": "게시물 상태가 토글되었습니다."}, status=status.HTTP_200_OK)
-        else:
-            return Response(
-                {"message": "자신의 게시물이 아닙니다."}, status=status.HTTP_403_FORBIDDEN
-            )
-
 
 class CommentAPIView(APIView):
     def get_object(self, pk):
@@ -123,14 +103,13 @@ class CommentAPIView(APIView):
                 {"message": "자신의 댓글이 아닙니다."}, status=status.HTTP_403_FORBIDDEN
             )
 
-
 class CommentListAPIView(APIView):  ## auther_id 도 나와야함
     def get(self, request):  # 모든 게시물
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, pk):
+    def post(self, request):
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
