d77a3f58f60f277324ba7ae52280b1c6c4cd41ef
khj <hhbb0230@naver.com>
2023-11-25 20:34:53 +0900

    Feat : Kakao login 닉네임, 프로필 사진 DB에 추가 기능 구현

--- qna/apps.py
+++ qna/apps.py
@@ -3,8 +3,4 @@ from django.apps import AppConfig
 
 class QnaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-<<<<<<< HEAD
     name = "qna"
-=======
-    name = "qna"
->>>>>>> develop
