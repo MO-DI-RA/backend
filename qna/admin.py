90e2fe36a01ba576cc751e277735e1822e1db1d6
khj <hhbb0230@naver.com>
2023-11-25 20:34:36 +0900

    feat : gathering Model 에 추가 필드 생성

--- qna/admin.py
+++ qna/admin.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import QnAPost, Answer
-
-# Register your models here.
-admin.site.register(QnAPost)
-admin.site.register(Answer)
-=======
 from .models import QnAPost, Answer, InterestedPost
 
 # Register your models here.
 admin.site.register(QnAPost)
 admin.site.register(Answer)
 admin.site.register(InterestedPost)
->>>>>>> develop
