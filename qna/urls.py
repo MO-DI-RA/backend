4edcba60755c817a911684496c8a3c101b7d0546
khj <hhbb0230@naver.com>
2023-11-29 12:55:55 +0900

    Branch : 기능별 브랜치 병합

--- qna/urls.py
+++ qna/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
 from .views import *
 
-app_name = "main"
 
 urlpatterns = [
     path("posts/", ListAPIView.as_view()),
     path("posts/<int:pk>/", PostAPIView.as_view()),
-    path("posts/<int:pk>/comments/", CommentListAPIView.as_view()),
-    path("posts/<int:pk>/comments/<int:comment_pk>/", CommentAPIView.as_view()),
+    path("posts/<int:pk>/comments/", AnswerListAPIView.as_view()),
+    path("posts/<int:pk>/comments/<int:comment_pk>/", AnswerAPIView.as_view()),
+    path(
+        "posts/<int:pk>/comments/<int:comment_pk>/reply/",
+        AnswerCommentListAPIView.as_view(),
+    ),
+    path(
+        "posts/<int:pk>/comments/<int:comment_pk>/reply/<int:reply_pk>/",
+        AnswerCommentAPIView.as_view(),
+    ),
+    path("posts/<int:pk>/toggle/", PostToggleStatus.as_view()),
     path("posts/search/", PostViewSet.as_view()),
-    path("posts/myposts/", UserPostAPIView.as_view()),
-    path("posts/<int:post_id>/like/", LikeAPIView.as_view()),
+    # path("posts/<int:post_id>/interest/", InterestedPost.as_view()),
     path("posts/interest/", UserInterestListAPI.as_view()),
+    path("posts/<int:post_id>/like/", LikeAPIView.as_view()),
+    path("posts/myposts/", UserPostAPIView.as_view()),
 ]
