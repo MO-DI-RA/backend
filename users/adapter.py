dfd625c30b99a5930e32148174054af3efd1c7d6
khj <hhbb0230@naver.com>
2023-11-29 13:02:22 +0900

    Rename: gathering app 에서 users 앱으로 user model 이동

--- users/adapter.py
+++ users/adapter.py
@@ -1,17 +1,6 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
-<<<<<<< HEAD
-class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    def save_user(self, request, sociallogin, form=None):
-        user = super().save_user(request, sociallogin, form)
-        oauth_data = sociallogin.account.extra_data.get("kakao_account").get("profile")
-        print("-----------------------------------", oauth_data)
-        user.nickname = oauth_data.get("nickname")
-        user.profile_image = oauth_data.get("profile_image_url")  # 뭔가 이상하게 url이 파싱됨
-        user.save()
-        return user
-=======
 class KakaoSocialAccountAdapter(DefaultSocialAccountAdapter):
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
@@ -23,4 +12,3 @@ class KakaoSocialAccountAdapter(DefaultSocialAccountAdapter):
         )  # 뭔가 이상하게 url이 파싱됨 이미지는 저장 안해도 될지도
         user.save()
         return user
->>>>>>> develop
